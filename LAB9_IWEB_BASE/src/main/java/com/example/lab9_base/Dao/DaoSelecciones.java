de7539d3ecebb380d317e7b97a51c08e0c6570b4
Gil Zanabria <108200371+xTarantino28@users.noreply.github.com>
2022-11-10 05:01:09 -0500

    listarSelecciones

--- LAB9_IWEB_BASE/src/main/java/com/example/lab9_base/Dao/DaoSelecciones.java
+++ LAB9_IWEB_BASE/src/main/java/com/example/lab9_base/Dao/DaoSelecciones.java
@@ -1,16 +1,42 @@
 package com.example.lab9_base.Dao;
 
-import com.example.lab9_base.Bean.Seleccion;
-
+import com.example.lab9_base.Bean.*;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class DaoSelecciones extends DaoBase{
     public ArrayList<Seleccion> listarSelecciones() {
 
-        ArrayList<Seleccion> selecciones = new ArrayList<>();
-        /*
-        Inserte su código aquí
-        */
+       ArrayList<Seleccion> selecciones = new ArrayList<>();
+
+        try(Connection conn = getConnection();
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM seleccion selec inner join estadio e on e.idEstadio = selec.estadio_idEstadio ;")) {
+
+            while(rs.next()){
+                Seleccion seleccion = new Seleccion();
+
+                seleccion.setIdSeleccion(rs.getInt(1));
+                seleccion.setNombre(rs.getString(2));
+                seleccion.setTecnico(rs.getString(3));
+
+
+                Estadio estadio = new Estadio();
+
+                estadio.setIdEstadio(rs.getInt("e.idEstadio"));
+                estadio.setNombre(rs.getString("e.nombre"));
+                estadio.setProvincia(rs.getString("e.provincia"));
+                estadio.setClub(rs.getString("e.club"));
+                seleccion.setEstadio(estadio);
+
+
+                selecciones.add(seleccion);
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return selecciones;
     }
 
