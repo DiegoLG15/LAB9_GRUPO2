4daaebf215fd5cc31045d9b6a08068cdb0cb6b6e
Gil Zanabria <108200371+xTarantino28@users.noreply.github.com>
2022-11-09 21:22:54 -0500

    listaPartidos

--- LAB9_IWEB_BASE/src/main/java/com/example/lab9_base/Dao/DaoPartidos.java
+++ LAB9_IWEB_BASE/src/main/java/com/example/lab9_base/Dao/DaoPartidos.java
@@ -8,42 +8,43 @@ public class DaoPartidos {
     public ArrayList<Partido> listaDePartidos() {
 
         ArrayList<Partido> partidos = new ArrayList<>();
-        /*
+
+
         try(Connection conn = getConnection();
             Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery("SELECT * FROM magenta.enemigos e\n" +
-                    "inner join clases_enemigos clase on e.idClaseEnemigo = clase.idClaseEnemigo\n" +
-                    "left join genero g on e.idGenero = g.idGenero\n" +
-                    "left join objetos o on e.idObjeto = o.idObjeto where e.borradoLogico=0 order by e.idEnemigo;")) {
+            ResultSet rs = st.executeQuery("SELECT p.idPartido, p.numeroJornada, p.fecha, seleclocal.nombre, selecvisit.nombre, esta.nombre, ar.nombre FROM lab9.partido p\n" +
+                    "left join seleccion seleclocal on seleclocal.idSeleccion = p.seleccionLocal \n" +
+                    "left join seleccion selecvisit on selecvisit.idSeleccion = p.seleccionVisitante \n" +
+                    "left join estadio esta on esta.idEstadio = seleclocal.estadio_idEstadio\n" +
+                    "left join arbitro ar on p.arbitro = ar.idArbitro\n" +
+                    "order by p.idPartido;")) {
 
             while(rs.next()){
-                Enemigo enemigo = new Enemigo();
+                Partido partido = new Partido();
 
-                enemigo.setIdEnemigo(rs.getInt(1));
-                enemigo.setNombreEnemigo(rs.getString(2));
-                enemigo.setAtaque(rs.getInt(3));
-                enemigo.setExperienciaDerrotado(rs.getInt(4));
-                enemigo.setProbDejarObjeto(rs.getDouble(5));
+                partido.setIdPartido(rs.getInt(1));
+                partido.setNumeroJornada(rs.getInt(2));
+                partido.setFecha(rs.getString(3));
 
-                Genero genero = new Genero();
-                genero.setIdGenero(rs.getInt("g.idGenero"));
-                genero.setInicial(rs.getString("inicial"));
-                enemigo.setGenero(genero);
+                Seleccion seleccionLocal = new Seleccion();
+                seleccionLocal.setNombre(rs.getString("seleclocal.nombre"));
+                Estadio estadioLocal = new Estadio();
+                estadioLocal.setNombre(rs.getString("esta.nombre"));
+                seleccionLocal.setEstadio(estadioLocal);
+                partido.setSeleccionLocal(seleccionLocal);
 
-                Objeto objeto = new Objeto();
-                objeto.setIdObjeto(rs.getInt("o.idObjeto"));
-                objeto.setNombreObjeto(rs.getString("nombreObjeto"));
-                objeto.setEfecto(rs.getString("efecto"));
-                objeto.setPeso(rs.getFloat("peso"));
-                enemigo.setObjeto(objeto);
 
-                ClaseEnemigo claseEnemigo = new ClaseEnemigo();
-                claseEnemigo.setIdClaseEnemigo(rs.getInt("clase.idClaseEnemigo"));
-                claseEnemigo.setNombreClase(rs.getString("nombreClase"));
-                enemigo.setClaseEnemigo(claseEnemigo);
-                enemigo.setBorradoLogico(rs.getInt("borradoLogico"));
+                Seleccion seleccionVisitante = new Seleccion();
+                seleccionVisitante.setNombre(rs.getString("selecvisit.nombre"));
+                partido.setSeleccionVisitante(seleccionVisitante);
 
-                listaEnemigos.add(enemigo);
+
+                Arbitro arbitro = new Arbitro();
+                arbitro.setNombre(rs.getString("ar.nombre"));
+                partido.setArbitro(arbitro);
+
+
+                partidos.add(partido);
 
             }
 
@@ -51,9 +52,6 @@ public class DaoPartidos {
             e.printStackTrace();
         }
 
-        return listaEnemigos;
-    }
-        */
         return partidos;
     }
 
